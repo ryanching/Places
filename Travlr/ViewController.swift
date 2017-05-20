97e51d860cfe27a6a123f4dda47e2146253f83df
Ryan Ching <ryanching@Ryans-MacBook-Pro.local>
2017-05-19 22:23:25 -0400

    Initial Commit

---
+++ Travlr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travlr
+//
+//  Created by Ryan Ching on 5/19/17.
+//  Copyright © 2017 Ryan Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
